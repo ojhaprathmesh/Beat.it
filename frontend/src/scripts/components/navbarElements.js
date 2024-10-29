09a4bed5b9c7f7d6bc540f8a326f0bb5b7d4b7de
Prathmesh Ojha <prathmeshojha2307@gmail.com>
2024-10-29 18:55:27 +0530

    Update navbarElements.js

--- frontend/src/scripts/components/navbarElements.js
+++ frontend/src/scripts/components/navbarElements.js
@@ -1,33 +1,48 @@
-function extendNavbar() {
+function renderLogo() {
     return `
-    <div id="logo">
-        <img src="../assets/Beat.it White Logo.webp" alt="Beat.it Logo" width="60px" height="60px">
-    </div>
-
-    <form class="searchbar" action="SearchPage.html">
-        <img id="search-icon" src="../assets/home/Search.webp" alt="Search Icon" width="32px" height="32px">
-        <hr id="line-separator">
-        <input class="font-inter" id="search-input" type="text" placeholder="Search by artists, songs or albums" style="font-weight: 100;">
-    </form>
-
-    <div class="menu font-inter">
-        <h1 id="username-letter" style="font-weight: 100;">S</h1>
-
-        <div class="overlay">
-            <p id="profile">Profile</p>
-            <div>
-                <hr style="width: 90%; border: 1px solid #3E3E3E">
-            </div>
-            <p id="logout">Log Out</p>
+        <div id="logo">
+            <img src="../assets/Beat.it White Logo.webp" alt="Beat.it Logo" width="60px" height="60px">
         </div>
-    </div>
+    `;
+}
+
+function renderSearchBar() {
+    return `
+        <form class="searchbar" action="SearchPage.html">
+            <img id="search-icon" src="../assets/home/Search.webp" alt="Search Icon" width="32px" height="32px">
+            <hr id="line-separator">
+            <input class="font-inter" id="search-input" type="text" placeholder="Search by artists, songs or albums" style="font-weight: 100;">
+        </form>
+    `;
+}
+
+function renderMenu() {
+    return `
+        <div class="menu font-inter">
+            <h1 id="username-letter" style="font-weight: 100;">S</h1>
+            <div class="overlay">
+                <p id="profile">Profile</p>
+                <div>
+                    <hr style="width: 90%; border: 1px solid #3E3E3E">
+                </div>
+                <p id="logout">Log Out</p>
+            </div>
+        </div>
+    `;
+}
+
+function createNavbar() {
+    return `
+        ${renderLogo()}
+        ${renderSearchBar()}
+        ${renderMenu()}
     `;
 }
 
 function insertNavbar(containerSelector) {
     const container = document.querySelector(containerSelector);
     if (container) {
-        container.innerHTML += extendNavbar();
+        container.innerHTML += createNavbar();
     }
 }
 
