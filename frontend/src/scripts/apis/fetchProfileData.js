7f44566095dc548ed350baadbac2935151582fe2
Prathmesh Ojha <prathmeshojha2307@gmail.com>
2024-11-04 19:44:22 +0530

    Delete fetchProfileData.js
    There was no need for this code!

--- frontend/src/scripts/apis/fetchProfileData.js
+++
@@ -1,6 +0,0 @@
-function fetchProfileData() {
-    const storedProfiles = localStorage.getItem('profiles');
-    return storedProfiles ? JSON.parse(storedProfiles) : [];
-}
-
-export { fetchProfileData };
