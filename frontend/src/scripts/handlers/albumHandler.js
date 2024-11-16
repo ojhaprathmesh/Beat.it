76921e58d54e3b8160cab511ee979feb5a472974
Prathmesh Ojha <prathmeshojha2307@gmail.com>
2024-11-16 17:26:09 +0530

    Update albumHandler.js

--- frontend/src/scripts/handlers/albumHandler.js
+++ frontend/src/scripts/handlers/albumHandler.js
@@ -1,10 +1,11 @@
 import { fetchSongData } from "../utility/fetchSongData.js";
 import { insertAlbums } from "../components/album.js";
+import { shuffle } from "../utility/shuffle.js";
 
 document.addEventListener("DOMContentLoaded", async () => {
     const songData = await fetchSongData();
 
-    await insertAlbums(".album-row", songData);
+    await insertAlbums(".album-row", shuffle(songData));
 
     document.querySelectorAll(".album-row-item").forEach(album => {
 
