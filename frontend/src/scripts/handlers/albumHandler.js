d453b109eef49216e826b15ab52ef117277c71f5
Prathmesh Ojha <prathmeshojha2307@gmail.com>
2024-11-22 19:51:44 +0530

    Updated styling for songs and albums!

--- frontend/src/scripts/handlers/albumHandler.js
+++ frontend/src/scripts/handlers/albumHandler.js
@@ -3,15 +3,23 @@ import { insertAlbums } from "../components/album.js";
 import { shuffle } from "../utility/shuffle.js";
 
 document.addEventListener("DOMContentLoaded", async () => {
-    const songData = await fetchSongData();
+    if (["/home", "/search"].includes(window.location.pathname)) {
+        const songData = await fetchSongData();
+        const shuffledSongs = shuffle(songData);
 
-    await insertAlbums(".album-row", shuffle(songData));
+        await insertAlbums(".album-row", shuffledSongs);
 
-    document.querySelectorAll(".album-row-item").forEach(album => {
-        album.addEventListener("click", () => {
-            document.dispatchEvent(new CustomEvent("albumClicked", {
-                detail: album.querySelector("img").alt
-            }));
+        document.querySelectorAll(".album-row-item").forEach(album => {
+            album.addEventListener("click", async () => {
+                const albumName = album.querySelector("img").alt;
+                const songsToPlay = shuffledSongs.filter(song => song.album === albumName);
+            });
         });
-    });
-})
+
+        if (window.location.pathname === "/search") {
+            document.querySelectorAll(".song-row-item").forEach((item) => { 
+                item.style.left = "250%";
+            })
+        }
+    }
+});
