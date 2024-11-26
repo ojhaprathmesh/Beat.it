4afd8b8daf2a7badd5c7ca3dd057db534f5bb67f
Prathmesh Ojha <prathmeshojha2307@gmail.com>
2024-11-26 14:16:27 +0530

    Update fetchSongData.js
    Added duration to all the songs!

--- frontend/src/scripts/utility/fetchSongData.js
+++ frontend/src/scripts/utility/fetchSongData.js
@@ -1,10 +1,31 @@
+function formatTime(seconds) {
+    const minutes = Math.floor(seconds / 60); // Extract the minutes
+    const remainingSeconds = Math.floor(seconds % 60); // Extract the remaining seconds
+    return `${minutes}:${remainingSeconds.toString().padStart(2, '0')}`;
+}
+
 async function fetchSongData() {
     try {
         const response = await fetch("http://localhost:3000/api/data/songsData");
         if (!response.ok) {
             throw new Error(`HTTP error! Status: ${response.status}`);
         }
-        return await response.json();
+        const data = await response.json();
+
+        const getDuration = (file) => {
+            return new Promise((resolve) => {
+                const audio = new Audio(file);
+                audio.addEventListener("loadedmetadata", () => {
+                    resolve(audio.duration);
+                });
+            });
+        };
+
+        for (const song of data) {
+            song.duration = formatTime(await getDuration(song.file));
+        }
+
+        return data;
     } catch (error) {
         console.warn("Failed to fetch song data:", error);
         return []; // Returning an empty array to handle the error!
