e69bf46ab6641371693c37a0f3a6419ad8420f12
jaisw07 <135193092+jaisw07@users.noreply.github.com>
2024-11-30 21:47:26 +0530

    set paths for public and embjsFiles folder

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,8 @@ const app = express();
 const port = 3000;
 
 app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+app.set("views", path.join(__dirname, "embjsFiles"));
 
 let albumSongs = [];
 
