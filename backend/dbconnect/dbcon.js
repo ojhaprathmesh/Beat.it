67068499f9f1d4847a45ffa10ae9db3e2c6df535
Prathmesh Ojha <prathmeshojha2307@gmail.com>
2024-12-14 22:21:03 +0530

    Refactored code!

--- backend/dbconnect/dbcon.js
+++ backend/dbconnect/dbcon.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const dbHost = process.env.DB_HOST;
 const dbName = process.env.DB_NAME;
 const dbUser = process.env.DB_USER;
-const dbPassword = process.env.DB_PASSWORD;
+const dbPass = process.env.DB_PASSWORD;
 
 const dbURI = `mongodb+srv://${dbUser}:${dbPassword}@${dbHost}/${dbName}?retryWrites=true&w=majority&appName=Site-Database`;
 
